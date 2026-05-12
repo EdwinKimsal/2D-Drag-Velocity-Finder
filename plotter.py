32adb8b2a5c5eab3a6994ed248351ef1a42f7b2b
Edwin Kimsal <kimsal.edwin@gmail.com>
2026-05-12 16:02:17 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 # Variables
 FINAL_X = 2.432 # Meters
-FINAL_Y = .1209 # Meters
+FINAL_Y = .4209 # Meters
 ANGLE = math.radians(45) # Radians
 
 # Constants
-MASS = 0.040 # Kilograms
+MASS = 0.0027 # Kilograms
 BALL_RADIUS = 0.02 # Meters
 AIR_DENSITY = 1.225 # kg/m^3
 DRAG_COEF = 0.50
@@ -18,119 +18,116 @@ DIFF_FACTOR = 0.75
 MAX_ITER = 100
 
 area = math.pi * BALL_RADIUS**2
-terminal_velocity = math.sqrt((2 * MASS * GRAVITY) / (area * AIR_DENSITY * DRAG_COEF))
+drag_factor = (0.5 * AIR_DENSITY * DRAG_COEF * area) / MASS
 init_vel = (FINAL_X / math.cos(ANGLE)) * math.sqrt(GRAVITY / (2 * (FINAL_X * math.tan(ANGLE) - FINAL_Y)))
 
 
-def get_point_reg(time, v0_x, v0_y):
-    del_x = v0_x * time
-    del_y = v0_y * time - 0.5 * GRAVITY * (time**2)
-
-    return {"del_x": del_x, "del_y": del_y}
-
-
-def get_point_air(time, v0_x, v0_y):
-    del_x = (terminal_velocity**2 / GRAVITY) * math.log(1 + (GRAVITY / terminal_velocity**2) * v0_x * time)
-
-    try:
-        del_y = (terminal_velocity**2 / GRAVITY) * math.log((math.cos(math.atan(v0_y / terminal_velocity) - (GRAVITY / terminal_velocity) * time)) / (math.cos(math.atan(v0_y / terminal_velocity))))
+# Get the path and distance of the object to the goal
+def get_drag_path_info(vel, plot=False, color="blue", label=None):
+    # Set initial point as the origin
+    pos_x = 0
+    pos_y = 0
+    label_drawn = False
     
-    except ValueError:
-        del_y = (terminal_velocity**2 / GRAVITY) * math.log((math.cosh(math.atanh(v0_y / terminal_velocity) + (GRAVITY / terminal_velocity) * time)) / (math.cosh(math.atanh(v0_y / terminal_velocity))))
-
-    return {"del_x": del_x, "del_y": del_y}
-
-
-
-def find_reg(plot=False):
-    # Init function vars
-    curr_x = 0
-    curr_y = 0
-    time = 0
-
-    # Find needed vars
-    v0_x = init_vel * math.cos(ANGLE)
-    v0_y = init_vel * math.sin(ANGLE)
-
-    # Iterate until it reaches the goal x position
-    while True:
-        points = get_point_reg(time, v0_x, v0_y)
-
-        curr_x = points["del_x"]
-        curr_y = points["del_y"]
-
-        if (plot):
-            plt.scatter(curr_x, curr_y, color="lime")
-
-        if curr_x >= FINAL_X:
-            break
-
-        time += TIME_DIFF
-
-
-def find_air(vel, plot=False):
-    # Init function vars
-    curr_x = 0
-    curr_y = 0
-    time = 0
-
-    # Find needed vars
-    v0_x = vel * math.cos(ANGLE)
-    v0_y = vel * math.sin(ANGLE)
-
-    # Iterate until it reaches the goal x position
-    while True:
-        points = get_point_air(time, v0_x, v0_y)
-
-        curr_x = points["del_x"]
-        curr_y = points["del_y"]
-
-        if (plot):
-            plt.scatter(curr_x, curr_y, color="blue")
-
-        if curr_x >= FINAL_X:
-            break
-
-        time += TIME_DIFF
-
-    dist = FINAL_Y - curr_y
-    return {"vel": vel, "dist": dist}
-
-
-def update_shot(shot_info):
-    if abs(shot_info["dist"]) <= DELTA_GOAL:
-        pass
-    else:
-        shot_info["vel"] += shot_info["dist"] * DIFF_FACTOR
+    # Calculate directional velocities
+    vel_x = vel * math.cos(ANGLE)
+    vel_y = vel * math.sin(ANGLE)
     
-    return shot_info    
+    while ((pos_y >= 0 or pos_y >= FINAL_Y) and pos_x < FINAL_X):
+        # Calculate accelerations
+        acc_x = -drag_factor * vel * vel_x
+        acc_y = -GRAVITY - drag_factor * vel * vel_y
+
+        # Update velocities
+        vel_x =  vel_x + (acc_x * TIME_DIFF)
+        vel_y =  vel_y + (acc_y * TIME_DIFF)
+
+        # Update positions
+        pos_x = pos_x + (vel_x * TIME_DIFF)
+        pos_y = pos_y + (vel_y * TIME_DIFF)
+
+        # Plot if needed
+        if plot:
+            if label and not label_drawn:
+                plt.scatter(pos_x, pos_y, color=color, label=label)
+                label_drawn = True
+            else:
+                plt.scatter(pos_x, pos_y, color=color)
+
+    # Return the final distance
+    dist = FINAL_Y - pos_y
+    return dist
 
 
+# Get the path of the object to the goal
+def get_reg_path_info(vel, plot=False, color="lime", label=None):
+    # Set initial point as the origin
+    pos_x = 0
+    pos_y = 0
+    label_drawn = False
+
+    # Calculate directional velocities
+    vel_x = vel * math.cos(ANGLE)
+    vel_y = vel * math.sin(ANGLE)
+
+    while pos_x < FINAL_X and pos_y >= 0:
+        # Calculate accelerations without air drag
+        acc_x = 0
+        acc_y = -GRAVITY
+
+        # Update velocities
+        vel_x = vel_x + (acc_x * TIME_DIFF)
+        vel_y = vel_y + (acc_y * TIME_DIFF)
+
+        # Update positions
+        pos_x = pos_x + (vel_x * TIME_DIFF)
+        pos_y = pos_y + (vel_y * TIME_DIFF)
+
+        # Plot if needed
+        if plot:
+            if label and not label_drawn:
+                plt.scatter(pos_x, pos_y, color=color, label=label)
+                label_drawn = True
+            else:
+                plt.scatter(pos_x, pos_y, color=color)
+    
+    return
+
+
+# Update the velocity of the shot
+def update_shot(vel, dist):
+    vel = vel + (dist * DIFF_FACTOR)
+    return vel  
+
+
+# Use functions
 def main():  
-    shot_info = {
-        "vel": init_vel,
-        "dist": math.inf
-    }
+    vel = init_vel
+    dist = math.inf
 
     # Plot the ideal scenario path
-    find_reg(True)
+    get_reg_path_info(vel, True, "lime", "No drag")
     
-    for _ in range(MAX_ITER):
-        shot_info = find_air(shot_info["vel"])
-        shot_info = update_shot(shot_info)
-        if abs(shot_info["dist"]) <= DELTA_GOAL:
+    for i in range(MAX_ITER):
+        dist = get_drag_path_info(vel)
+        if (abs(dist) > DELTA_GOAL):
+            vel = update_shot(vel, dist)
+        else:
             break
 
     # Plot the air resistance path
-    find_air(shot_info["vel"], True)
+    get_drag_path_info(vel, True, "blue", "Quadratic drag")
 
     # Plot the goal
-    plt.scatter(FINAL_X, FINAL_Y, color="red")
+    plt.scatter(FINAL_X, FINAL_Y, color="red", label="Target")
 
-    # Initialize graph and show it  
+    # Initialize graph and show it
     plt.title("Path Traveled")
     plt.xlabel("X Distance")
     plt.ylabel("Y Distance")
+    plt.gca().set_aspect("equal", adjustable="box")
+    plt.margins(0.20)
+    plt.legend(fontsize="xx-small", loc="upper right")
     plt.show()
 
 
